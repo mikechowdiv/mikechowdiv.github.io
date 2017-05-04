461e680894b065eeeb6717ef51a38d0bf6914cc3
Mike Chow <mikechowdiv@gmail.com>
2017-05-04 15:13:11 +0800

    updated

--- script.js
+++ script.js
@@ -26,3 +26,46 @@ function getAvatarColor()
 {
   return "#bdc3c7";
 }
+
+
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').parent().removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).parent().addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
+    }
+  });
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 750);
+  
+  return false;
+});
+
+
+$(document).ready(function() {
+    $('#bt1').click(function() {
+        $('#fr1').attr('action',
+                       'mailto:mikechowdiv@gmail.com?subject=' +
+                       $('#usr').val() + '&body=' + $('#msg').val());
+        $('#fr1').submit();
+    });
+});
